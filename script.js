ec226caaeb832d523e9c1ed418705aa35abfa75f
Francisco Nailton <naylton.arruda@gmail.com>
2024-06-17 20:29:22 -0300

    Commit Final

---
+++ script.js
@@ -0,0 +1,65 @@
+let emailSalvo 
+let senhaSalva 
+
+
+function formatPhone(input) {
+    let value = input.value.replace(/\D/g, '');
+    if (value.length > 2) {
+        value = `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7)}`;
+    }
+    input.value = value;
+}
+
+function validateEmail(email) {
+
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+        alert('Por favor, insira um e-mail válido.');
+        return false;
+    }
+    return true;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const formularioCadastro = document.getElementById("formulario_cadastro");
+    if (formularioCadastro) {
+        formularioCadastro.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const name = document.getElementById("nome").value;
+            const email = document.getElementById("email").value;
+            const emailValidado = validateEmail(email);
+            const senha = document.getElementById("senha").value;
+            const confirmarSenha = document.getElementById("confirmar_senha").value;
+
+            if(senha !== confirmarSenha){
+                alert('As senhas informadas estão divergentes.');
+            } else {
+                localStorage.setItem('email', email);
+                localStorage.setItem('senha', senha);
+                window.location.href = "/cadastrologin/login.html";
+            }
+        });
+    }
+
+    const formularioLogin = document.getElementById("formulario_login");
+    if (formularioLogin) {
+        formularioLogin.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const email = document.getElementById("email").value;
+            const senha = document.getElementById("senha").value;
+            const emailSalvo = localStorage.getItem('email');
+            const senhaSalva = localStorage.getItem('senha');
+            if(email !== emailSalvo || senha !== senhaSalva){
+                alert('As senhas informadas estão divergentes.');
+            } else {
+                window.location.href = "/home/home.html";
+            }
+        });
+    }
+});
+
+function toggleText(element) {
+    element.classList.toggle('expanded');
+}
